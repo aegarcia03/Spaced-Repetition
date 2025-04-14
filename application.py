375e8231a323f81c8b4157badd8e03a049a54d9d
Angela Enriquez <angelaenriquezgar@gmail.com>
2025-04-14 14:54:45 +1000

    adding requirements

--- application.py
+++ application.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template, request
 from slot_machine import SlotMachine, Game, Player, Symbol
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@application.route('/', methods=['GET', 'POST'])
 def index():
     #will store the spin results
     result = None
     balance = 0
 
     if request.method == 'POST':
-        initial_balance = int(request.form.get['balance', 0])
-        lines = int(request.form.get['lines', 1])
-        bet = int(request.form.get['bet', 1])
+        initial_balance = int(request.form.get('balance', 0))
+        lines = int(request.form.get('lines', 1))
+        bet = int(request.form.get('bet', 1))
 
         print("Form submitted:", request.form)
 
@@ -29,16 +29,17 @@ def index():
         player.balance += winnings - (bet * lines)
         balance = player.balance
         #store all the result into a dict so i can pass it on HTML template
-        resutl = {
+        result = {
             'columns': result,
             'winnings': winnings,
             'lines': winning_lines
         }
-        #Renders the index.html page and gives it the result data 
+        #Renders the index.html page and gives it the result data
+        total_bet = bet * lines 
         balance += winnings - total_bet
     
     return render_template('index.html', result=result, balance=balance)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True, host='0.0.0.0', port=8000)
